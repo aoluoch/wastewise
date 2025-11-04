82ac99f87c0ee630c762a3e9556b18d6ffc9537f
aoluoch <aoluoch271@gmail.com>
2025-11-04 14:42:48 +0300

    added a vite base url to the axios instance

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { ApiError } from '../types'
 
-const baseURL = '/api'
+const baseURL = import.meta.env.VITE_API_BASE_URL || '/api'
 
 export const axiosInstance = axios.create({
   baseURL,
